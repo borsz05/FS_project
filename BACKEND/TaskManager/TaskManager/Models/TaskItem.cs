e867d89f402020a016f725c7b416a709da56a564
Bertalan Tóth <bertalantoth2005@gmail.com>
2025-03-16 14:57:22 +0100

    TaskItem model implemented

---
+++ BACKEND/TaskManager/TaskManager/Models/TaskItem.cs
@@ -0,0 +1,17 @@
+﻿namespace TaskManager.Models
+{
+    public class TaskItem
+    {
+        public string Name { get; set; }
+        public int TotalMinutes { get; set; }
+        public int AvailableDays { get; set; }
+        public bool Divisible => AvailableDays > 1;
+
+        public TaskItem(string name, int totalHours, int availableDays)
+        {
+            Name = name;
+            TotalMinutes = totalHours * 60;
+            AvailableDays = availableDays;
+        }
+    }
+}
