c2d290516e481332e308e9b821eee60b0e4a982b
Bertalan Tóth <bertalantoth2005@gmail.com>
2025-03-16 15:53:45 +0100

    Add Scheduler class with ScheduleDays list and initialize with Day 1

---
+++ BACKEND/TaskManager/TaskManager/Models/Scheduler.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskManager.Models
+{
+    public class Scheduler
+    {
+        public List<DaySchedule> ScheduleDays { get; set; } = new List<DaySchedule>();
+
+        public Scheduler()
+        {
+            ScheduleDays.Add(new DaySchedule(1));
+        }
+    }
+}
