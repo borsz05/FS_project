c2f251e578a5ec2d3217a622e2ba13bbde5b7879
Bertalan Tóth <bertalantoth2005@gmail.com>
2025-03-16 15:20:56 +0100

    DaySchedule model implemented

---
+++ BACKEND/TaskManager/TaskManager/Models/DaySchedule.cs
@@ -0,0 +1,22 @@
+﻿namespace TaskManager.Models
+{
+    public class DaySchedule
+    {
+        public int DayNumber { get; set; }
+        public List<TaskAssignment> Assignments { get; set; } = new List<TaskAssignment>();
+
+        public const int Capacity = 600;
+        public const int BreakTime = 15;
+
+        public int TotalMinutes => Assignments.Where(a => a.Minutes > 0).Sum(a => a.Minutes);
+
+        public int EffectiveLoad => TotalMinutes + ((Assignments.Count > 0 ? Assignments.Count - 1 : 0) * BreakTime);
+
+        public int RemainingMinutes => Capacity - EffectiveLoad;
+
+        public DaySchedule(int dayNumber)
+        {
+            DayNumber = dayNumber;
+        }
+    }
+}
