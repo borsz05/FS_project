2ec7d9a700b81b9a71b72df40a66630645b5a75a
Bertalan Tóth <bertalantoth2005@gmail.com>
2025-03-16 16:54:58 +0100

    InsertTask implemented in SchedulerService

--- BACKEND/TaskManager/TaskManager/Services/SchedulerService.cs
+++ BACKEND/TaskManager/TaskManager/Services/SchedulerService.cs
@@ -11,6 +11,29 @@ namespace TaskManager.Services
             // Kezdjük az első nappal
             ScheduleDays.Add(new DaySchedule(1));
         }
+        private void EnsureDayExists(int dayNumber)
+        {
+            while (ScheduleDays.Count < dayNumber)
+                ScheduleDays.Add(new DaySchedule(ScheduleDays.Count + 1));
+        }
+        public void InsertTask(TaskItem task)
+        {
+            if (!task.Divisible)
+            {
+                // Nem darabolható: keressük az első olyan napot, ahol elfér (szünetek figyelembe véve)
+                foreach (var day in ScheduleDays)
+                {
+                    if (day.RemainingMinutes >= task.TotalMinutes + (day.Assignments.Count > 0 ? DaySchedule.BreakTime : 0))
+                    {
+                        day.Assignments.Add(new TaskAssignment(task.Name, task.TotalMinutes));
+                        return;
+                    }
+                }
+                int newDay = ScheduleDays.Count + 1;
+                EnsureDayExists(newDay);
+                ScheduleDays[newDay - 1].Assignments.Add(new TaskAssignment(task.Name, task.TotalMinutes));
+            }
+        }
 
     }
 }
