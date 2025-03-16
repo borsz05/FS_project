146a8fdf759d9d1dace7a3251dac861430457054
Bertalan Tóth <bertalantoth2005@gmail.com>
2025-03-16 15:04:27 +0100

    TaskAssignment model implemented

---
+++ BACKEND/TaskManager/TaskManager/Models/TaskAssignment.cs
@@ -0,0 +1,27 @@
+﻿namespace TaskManager.Models
+{
+    public class TaskAssignment
+    {
+        public string TaskName { get; set; }
+        public int Minutes { get; set; }
+        public bool IsDivisible { get; set; }
+        public int TaskStartDay { get; set; }
+        public int TaskAvailableDays { get; set; }
+
+        public TaskAssignment(string taskName, int minutes)
+        {
+            TaskName = taskName;
+            Minutes = minutes;
+            IsDivisible = false;
+        }
+
+        public TaskAssignment(string taskName, int minutes, int taskStartDay, int taskAvailableDays)
+        {
+            TaskName = taskName;
+            Minutes = minutes;
+            IsDivisible = true;
+            TaskStartDay = taskStartDay;
+            TaskAvailableDays = taskAvailableDays;
+        }
+    }
+}
